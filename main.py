33d64f16d5a60ec413533d4f35e52d15c7895853
Mugilan <mugilan.an@gmail.com>
2024-05-10 22:58:49 +0530

    Added FPS counter

--- main.py
+++ main.py
@@ -1,43 +1,58 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import time
 import math
 import pygame
 
 # Sim settings
-scrw = 1280                                         # screen width
-scrh = 720                                          # screen height
-bw = int(1e+2)                                      # board dims
-cellsize = 50                                       # default cell size in pixels
-cps = 5                                             # mvmt speed in cells per second
+scrw = 1280  # screen width
+scrh = 720  # screen height
+bw = int(1e+2)  # board dims
+cellsize = 50  # default cell size in pixels
+cps = 5  # mvmt speed in cells per second
 cellclr = {0: "Black",
            1: "Blue",
            2: "Green",
            3: "White"
-           }                                        # cell colors
-scrsplit = 0.7                                      # screen split - board | stats, eff 896px
-maxfps = int(60)                                    # max FPS limit
-margin = 2                                          # extra cells to render
-zoomsens = 1.008                                    # zoom sensitivity, multiplicative
+           }  # cell colors
+scrsplit = 0.7  # screen split - board | stats, eff 896px
+maxfps = int(60)  # max FPS limit
+margin = 2  # extra cells to render
+zoomsens = 1.008  # zoom sensitivity, multiplicative
 
 # Initialize pygame & game setup:
-pygame.init()                                       # screen height
+pygame.init()
 screen = pygame.display.set_mode((scrw, scrh))
+pygame.display.set_caption("Cellular Automation Simulator")
+font = pygame.font.Font('freesansbold.ttf', 32)
 clock = pygame.time.Clock()
+currfps = font.render('Current FPS: ',
+                      True,
+                      "White",
+                      "Black"
+                      )
+currfpsRect = currfps.get_rect()
+currfpsRect.left = (scrw * scrsplit * 1.03)
+currfpsRect.top = (scrh * 0.05)
+fps = 0
+fpstext = font.render(str(fps), True, "White")
+fpsrect = fpstext.get_rect()
+fpsrect.left = currfpsRect.right * 1.03
+fpsrect.top = currfpsRect.top
 running = True
-dt = 0                                              # time elapsed (in ms)
+dt = 0  # time elapsed (in ms)
 i = 0
 
 bh = int(bw)
 board = np.zeros((bw, bh), dtype=np.int8)
-cells_h = (scrh / cellsize) + margin                # number of cells to render, w and h dims. float
+cells_h = (scrh / cellsize) + margin  # number of cells to render, w and h dims. float
 cells_w = ((scrw * scrsplit) / cellsize) + margin
 btl = [(bw / 2) - ((cells_w - margin) / 2) + 0.5,
        (bh / 2) - ((cells_h - margin) / 2) + 0.5
-       ]                                            # board top left, in board coords
+       ]  # board top left, in board coords
 scrtl = [int(btl[0] * cellsize),
          int(btl[1] * cellsize)
-         ]                                          # screen top left, in pixels.
+         ]  # screen top left, in pixels.
 
 # Initial state
 for i in range(bw):
@@ -66,6 +81,9 @@ while running:
     cell_right = cell_left + int(cells_w)
     cell_top = math.floor(btl[1])
     cell_bottom = cell_top + int(cells_h)
+
+    fps = int(clock.get_fps())
+    fpstext = font.render(str(fps), True, "White")
 
     for wc in range(cell_left, cell_right, 1):
         for hc in range(cell_top, cell_bottom, 1):
@@ -102,6 +120,8 @@ while running:
                                  scrh
                                  )
                      )
+    screen.blit(currfps, currfpsRect)
+    screen.blit(fpstext, fpsrect)
 
     # Input handling:
     keys = pygame.key.get_pressed()
