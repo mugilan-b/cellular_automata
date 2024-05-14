Revamped camera position, added more info text, fixed boundary conditions/overflow.

main.py
@@ -27,6 +27,7 @@ screen = pygame.display.set_mode((scrw, scrh))
 pygame.display.set_caption("Cellular Automation Simulator")
 font = pygame.font.Font('freesansbold.ttf', 32)
 clock = pygame.time.Clock()
+
 currfps = font.render('Current FPS: ',
                       True,
                       "White",
@@ -36,24 +37,37 @@ currfpsRect = currfps.get_rect()
 currfpsRect.left = (scrw * scrsplit * 1.03)
 currfpsRect.top = (scrh * 0.05)
 fps = 0
-fpstext = font.render(str(fps), True, "White")
-fpsrect = fpstext.get_rect()
-fpsrect.left = currfpsRect.right * 1.03
-fpsrect.top = currfpsRect.top
+fpstext = font.render(str(fps),
+                      True,
+                      "White",
+                      "Black")
+fpsRect = fpstext.get_rect()
+fpsRect.left = currfpsRect.right * 1.01
+fpsRect.top = currfpsRect.top
+cst = font.render('Scale: ',
+                  True,
+                  "White",
+                  "Black")
+cstRect = cst.get_rect()
+cstRect.left = (scrw * scrsplit * 1.03)
+cstRect.top = currfpsRect.bottom * 1.1
+csnt = font.render(str(cellsize),
+                   True,
+                   "White",
+                   "Black")
+csntRect = csnt.get_rect()
+csntRect.left = cstRect.right * 1.01
+csntRect.top = cstRect.top
+
 running = True
 dt = 0  # time elapsed (in ms)
-i = 0
-
 bh = int(bw)
 board = np.zeros((bw, bh), dtype=np.int8)
 cells_h = (scrh / cellsize) + margin  # number of cells to render, w and h dims. float
 cells_w = ((scrw * scrsplit) / cellsize) + margin
-btl = [(bw / 2) - ((cells_w - margin) / 2) + 0.5,
-       (bh / 2) - ((cells_h - margin) / 2) + 0.5
-       ]  # board top left, in board coords
-scrtl = [int(btl[0] * cellsize),
-         int(btl[1] * cellsize)
-         ]  # screen top left, in pixels.
+cam_pos = [(bw / 2) - ((cells_w - margin) / 2) + 0.5,
+           (bh / 2) - ((cells_h - margin) / 2) + 0.5
+           ]  # cam position, denotes top left in board coordinates
 
 # Initial state
 for i in range(bw):
@@ -67,7 +81,6 @@ for i in range(bh):
     board[0][i] = 3
     board[bw - 1][i] = 3
 board[50][50] = 2
-
 i = 0
 
 # Main game loop:
@@ -81,31 +94,30 @@ while running:
 
     screen.fill("gray25")
 
-    cell_left = math.floor(btl[0])
+    cell_left = max(math.floor(cam_pos[0]), 0)
     cell_right = cell_left + int(cells_w)
-    cell_top = math.floor(btl[1])
+    cell_top = max(math.floor(cam_pos[1]), 0)
     cell_bottom = cell_top + int(cells_h)
 
-    fps = int(clock.get_fps())
-    fpstext = font.render(str(fps), True, "White")
+    fps = clock.get_fps()
+    fpstext = font.render(str(f'{fps:.2f}'), True, "White", "Black")
+    csnt = font.render(str(f'{cellsize:.2f}'), True, "White", "Black")
 
-    for wc in range(cell_left, cell_right, 1):
-        for hc in range(cell_top, cell_bottom, 1):
-            bx = min(max(wc, 0), bw - 1)
-            by = min(max(hc, 0), bh - 1)
-            if board[bx][by] != 0:
+    for wc in range(cell_left, min(cell_right, bw), 1):
+        for hc in range(cell_top, min(cell_bottom, bh), 1):
+            if board[wc][hc] != 0:
                 pygame.draw.rect(screen,
-                                 cellclr[int(board[bx][by])],
-                                 pygame.Rect(int((wc * cellsize) - scrtl[0]),
-                                             int((hc * cellsize) - scrtl[1]),
+                                 cellclr[int(board[wc][hc])],
+                                 pygame.Rect(int((wc - cam_pos[0]) * cellsize),
+                                             int((hc - cam_pos[1]) * cellsize),
                                              int(cellsize),
                                              int(cellsize)
                                              )
                                  )
                 pygame.draw.rect(screen,
                                  "Black",
-                                 pygame.Rect(int((wc * cellsize) - scrtl[0]),
-                                             int((hc * cellsize) - scrtl[1]),
+                                 pygame.Rect(int((wc - cam_pos[0]) * cellsize),
+                                             int((hc - cam_pos[1]) * cellsize),
                                              int(cellsize),
                                              int(cellsize)
                                              ),
@@ -127,32 +139,28 @@ while running:
                                  )
                      )
     screen.blit(currfps, currfpsRect)
-    screen.blit(fpstext, fpsrect)
+    screen.blit(fpstext, fpsRect)
+    screen.blit(csnt, csntRect)
+    screen.blit(cst, cstRect)
 
     # Input handling:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        if btl[1] > cells_h / 2:
-            btl[1] -= cps * (dt / 1000)
+        cam_pos[1] -= cps * (dt / 1000)
     if keys[pygame.K_s]:
-        if btl[1] < bh - (cells_h / 2):
-            btl[1] += cps * (dt / 1000)
+        cam_pos[1] += cps * (dt / 1000)
     if keys[pygame.K_a]:
-        if btl[0] > cells_w / 2:
-            btl[0] -= cps * (dt / 1000)
+        cam_pos[0] -= cps * (dt / 1000)
     if keys[pygame.K_d]:
-        if btl[0] < bw - (cells_w / 2):
-            btl[0] += cps * (dt / 1000)
+        cam_pos[0] += cps * (dt / 1000)
     if keys[pygame.K_k]:
-        if cellsize > 20:
+        if cellsize > 1:
             cellsize /= zoomsens
             cps *= zoomsens
     if keys[pygame.K_j]:
-        if cellsize < 200:
-            cellsize *= zoomsens
-            cps /= zoomsens
+        cellsize *= zoomsens
+        cps /= zoomsens
 
-    scrtl = [int(btl[0] * cellsize), int(btl[1] * cellsize)]
     cells_h = (scrh / cellsize) + margin
     cells_w = ((scrw * scrsplit) / cellsize) + margin
 